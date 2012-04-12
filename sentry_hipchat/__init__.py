3dc5a2e8e9b20c258a5c1863d1258d51928dd285
Xavier Ordoquy <xordoquy@linovia.com>
2012-04-12 08:33:36 +0200

    sentry_hipchat.VERSION now gives the current version.

--- sentry_hipchat/__init__.py
+++ sentry_hipchat/__init__.py
@@ -5,3 +5,9 @@ sentry_hipchat
 :copyright: (c) 2011 by Linovia, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
+
+try:
+    VERSION = __import__('pkg_resources') \
+        .get_distribution('sentry_hipchat').version
+except Exception, e:
+    VERSION = 'unknown'
