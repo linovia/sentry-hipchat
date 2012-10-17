0c966f8b45134b54931b0693ae371a63f7bf500e
Xavier Ordoquy <xordoquy@linovia.com>
2012-10-17 04:24:36 +0200

    New release.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ install_requires = [
 
 setup(
     name='sentry-hipchat',
-    version='0.2.2',
+    version='0.2.3',
     author='Xavier Ordoquy',
     author_email='xordoquy@linovia.com',
     url='http://github.com/linovia/sentry-hipchat',
