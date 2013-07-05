08d2e6fae23c7bc8012d128a852d3b1d009f3b0a
Xavier Ordoquy <xordoquy@linovia.com>
2013-07-05 09:46:58 +0200

    And a fix for the current sentry.

--- sentry_hipchat/models.py
+++ sentry_hipchat/models.py
@@ -77,7 +77,7 @@ class HipchatMessage(NotifyPlugin):
         notify = self.get_option('notify', event.project) or False
         include_project_name = self.get_option('include_project_name', event.project) or False
         level = event.get_level_display().upper()
-        link = self.get_group_url(group)
+        link = self.get_url(group)
 
         if token and room:
             self.send_payload(token, room, '[%(level)s]%(project_name)s %(message)s %(link)s' % {
