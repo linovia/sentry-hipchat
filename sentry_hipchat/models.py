cf94c96960c6d52ffedeaeb0cac9cc5e8ab4d2b1
Xavier Ordoquy <xordoquy@linovia.com>
2013-07-05 09:47:27 +0200

    Slightly changed the message display.

--- sentry_hipchat/models.py
+++ sentry_hipchat/models.py
@@ -80,7 +80,7 @@ class HipchatMessage(NotifyPlugin):
         link = self.get_url(group)
 
         if token and room:
-            self.send_payload(token, room, '[%(level)s]%(project_name)s %(message)s %(link)s' % {
+            self.send_payload(token, room, '[%(level)s]%(project_name)s %(message)s [%(link)s]' % {
                 'level': level,
                 'project_name': (' <strong>%s</strong>' % event.project.name) if include_project_name else '',
                 'message': event.message,
